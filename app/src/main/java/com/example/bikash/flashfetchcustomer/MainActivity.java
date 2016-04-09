498ef76231daf66fb711f45fca5c3c3d62267097
SAM10795 <silversoulsam@gmail.com>
2016-04-09 15:54:54 +0530

    Added more layouts

--- app/src/main/java/com/example/bikash/flashfetchcustomer/MainActivity.java
+++ app/src/main/java/com/example/bikash/flashfetchcustomer/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startActivity(new Intent(this,Delivery.class));
+
         Login = (Button) findViewById(R.id.button_signin);
         Register = (Button) findViewById(R.id.button_register);
         Login.setOnClickListener(new View.OnClickListener() {

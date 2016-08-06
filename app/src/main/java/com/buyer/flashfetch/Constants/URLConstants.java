c95502f994153c989b12e90cf2ee443f82a5074e
KRANTHI <cascade.kiran.2010@gmail.com>
2016-08-06 18:52:45 +0530

    Major Changes

--- app/src/main/java/com/buyer/flashfetch/Constants/URLConstants.java
+++ app/src/main/java/com/buyer/flashfetch/Constants/URLConstants.java
@@ -4,7 +4,7 @@ package com.buyer.flashfetch.Constants;
  * Created by kevinselvaprasanna on 25/12/15.
  */
 public class URLConstants {
-    public static String SERVER = "http://ec2-54-169-112-228.ap-southeast-1.compute.amazonaws.com/";
+    public static String SERVER = "http://ec2-52-35-108-81.us-west-2.compute.amazonaws.com/";
 
     public static String URL_FETCH_PRODUCT = SERVER + "clink/";
 

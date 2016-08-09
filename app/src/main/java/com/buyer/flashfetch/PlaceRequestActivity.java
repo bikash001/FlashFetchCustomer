3b84b9f070187642005808e02d6bbe9651c2952c
KRANTHI <cascade.kiran.2010@gmail.com>
2016-08-10 00:28:59 +0530

    Major Changes

--- app/src/main/java/com/buyer/flashfetch/PlaceRequestActivity.java
+++ app/src/main/java/com/buyer/flashfetch/PlaceRequestActivity.java
@@ -2,16 +2,51 @@ package com.buyer.flashfetch;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by KRANTHI on 08-08-2016.
  */
 public class PlaceRequestActivity extends BaseActivity {
 
+    private Button setTimeButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.place_request);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Place Request");
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                finish();
+            }
+        });
+
+        Spinner spinner = (Spinner)findViewById(R.id.places_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.places_array, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+
+        setTimeButton = (Button)findViewById(R.id.place_request_time);
+        setTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
